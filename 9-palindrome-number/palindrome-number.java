a31a52aefc131950b1eeaa9e4d59ebb3d8fcce7e
Shivam Kaware <shivamkaware269@gmail.com>
2026-07-24 19:40:27 +0530

    Time: 4 ms (100.00%) | Memory: 45.6 MB (94.57%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            
+            return false;
+        }
+        int rev = 0, rem, temp = x;
+        while(temp != 0) {
+            rem = temp % 10;
+            rev = rev*10 + rem;
+            temp /= 10;
+        }
+        if(rev == x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
